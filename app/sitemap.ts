943dbb5dbcee8cf8e2482e24fcd6b79fa433554a
sanji <muthusanjay567@gmail.com>
2026-07-10 11:10:29 +0530

    Add sitemap and robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,21 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: "https://sanji-portfolio.vercel.app/#about",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://sanji-portfolio.vercel.app/#skills",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://sanji-portfolio.vercel.app/#projects",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://sanji-portfolio.vercel.app/#contact",
-      lastModified: new Date(),
-    },
   ];
 }
